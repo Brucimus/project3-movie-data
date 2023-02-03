090eae1e6f30563aacf57f6e3d69f47f03157a35
Brucimus <brucimus@gmail.com>
2023-02-02 21:29:11 -0800

    javascript csv data

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    print("view1")
-    return "view1"
+    return (
+        f"Welcome to the Hawaii Climate Analysis API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/temp/start<br/>"
+        f"/api/v1.0/temp/start/end<br/>"
+        f"<p>'start' and 'end' date should be in the format MMDDYYYY.</p>"
 
-@app.route("/view2")
-def home():
-    print("view2")
-    return "view2"
+    )
+    # return render_template('fview1.html')
 
-@app.route("/view3")
-def home():
-    print("view3")
-    return "view3"
+# @app.route("/view2")
+# def view2():
+#     return render_template('view2.html')
+
+# @app.route("/view3")
+# def view3():
+#     return render_template('view3.html')
---
+++ logic.js
@@ -0,0 +1,3 @@
+d3.csv("tmdb_5000_movies.csv",function(data){
+    console.log(data); 
+});
