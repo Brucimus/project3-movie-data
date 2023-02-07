10001280968993f63fc6f2141b2bf73812b81dbc
Brucimus <brucimus@gmail.com>
2023-02-06 21:32:02 -0800

    data in console

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+import pandas as pd
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./templates/', static_folder='./static')
 
 @app.route("/")
 def home():
-    return render_template('view1.html')
+    df = pd.read_csv('./tmdb_5000_credits.csv')
+    print(df.head())
+    return render_template('view1.html', data=df.to_dict())
 
 @app.route("/view2")
 def view2():
---
+++ static/logic.js
@@ -0,0 +1,9 @@
+d3.csv("tmdb_5000_movies.csv",function(data){
+    console.log(data); 
+});
+
+// d3.csv("tmdb_5000_credits.csv",function(data2){
+//     console.log(data2); 
+// });
+
+// app.use("/static", express.static('./static/'));
