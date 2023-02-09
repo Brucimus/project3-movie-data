68d0ef867e0656f1ab3dc74d55d5169d19461776
Geraldine <galongo@uci.edu>
2023-02-08 20:13:58 -0800

    updated sql file

--- table_schemata.sql
+++ table_schemata.sql
@@ -2,24 +2,20 @@
 DROP TABLE IF EXISTS movies CASCADE;
 --Create movies table
 CREATE TABLE movies (
+  title VARCHAR NOT NULL,
   budget INT NOT NULL,
-  genres VARCHAR(255) NOT NULL,
-  homepage VARCHAR(255) NOT NULL,
-  id INT,
-  keywords VARCHAR(255) NOT NULL,
-  original_language VARCHAR(255) NOT NULL,
-  original_title VARCHAR(255) NOT NULL,
-  overview VARCHAR(255) NOT NULL,
+  id SERIAL,
+  original_language VARCHAR NOT NULL,
+  original_title VARCHAR NOT NULL,
   popularity FLOAT,
-  production_companies VARCHAR(255) NOT NULL,
-  production_countries VARCHAR(255) NOT NULL,
+  production_companies VARCHAR NOT NULL,
+  production_countries VARCHAR NOT NULL,
   release_date DATE,
-  revenue INT,
-  runtime INT,
-  spoken_languages VARCHAR(255) NOT NULL,
-  status VARCHAR(255) NOT NULL,
-  tagline VARCHAR(255) NOT NULL,
-  title VARCHAR(255) NOT NULL,
+  revenue FLOAT,
+  runtime FLOAT,
+  spoken_languages VARCHAR NOT NULL,
+  status VARCHAR NOT NULL,
+  tagline VARCHAR NOT NULL,
   vote_average FLOAT,
   vote_count INT,
 	--set the id and the title columns as primary keys
@@ -30,12 +26,8 @@ CREATE TABLE movies (
 DROP TABLE IF EXISTS credits CASCADE;
 --Create credits table
 CREATE TABLE credits (
-  movie_id INT,
-  title VARCHAR(255) NOT NULL,
-  actors VARCHAR(255) NOT NULL,
-  crew VARCHAR(255) NOT NULL,
-	--set the movie_id column as a foreign key with id column from movies as reference
-  FOREIGN KEY (movie_id) REFERENCES movies (id),
-		--set the title column as a foreign key with title column from movies as reference
-	FOREIGN KEY (title) REFERENCES movies (title)
+  title VARCHAR NOT NULL,
+  movie_id SERIAL,
+  actors VARCHAR NOT NULL,
+  crew VARCHAR NOT NULL
 );
